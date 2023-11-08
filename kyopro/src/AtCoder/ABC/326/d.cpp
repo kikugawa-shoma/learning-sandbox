f396f90148c5c4549e4ae3e35e1718e66ab57911
Shoma Kikugawa <kikugawa.s.shukatsu@gmail.com>
2023-11-08 22:49:54 +0900

    add ac-library

--- kyopro/src/AtCoder/ABC/326/d.cpp
+++ kyopro/src/AtCoder/ABC/326/d.cpp
@@ -1,175 +1,57 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-void print_2vec(vector<vector<char>> V)
+// 2次元ベクターのprint関数
+template <typename T>
+void print_1_vec(vector<T> v)
 {
-  for (int h = 0; h < V.size() - 1; h++)
+  for (auto i = 0; i < v.size(); i++)
   {
-    for (int w = 0; w < V[h].size() - 1; w++)
+    cout << v[i] << ' ';
+  }
+  cout << endl;
+}
+
+// 2次元ベクターの関数
+template <typename T>
+void print_2_vec(vector<vector<T>> v)
+{
+  for (auto i = 0; i < v.size(); i++)
+  {
+    for (auto j = 0; j < v[i].size(); j++)
     {
-      cout << V[h][w] << ' ';
+      cout << v[i][j] << ' ';
     }
-    if (h != V.size() - 1)
+    if (i != v.size())
+    {
       cout << endl;
-  }
-}
-
-vector<vector<char>> write_map(vector<vector<char>> V)
-{
-  int N = (int)V.size();
-  vector<set<char>> row_remainings(N, {'A', 'B', 'C'});
-  vector<set<char>> column_remainings(N, {'A', 'B', 'C'});
-  for (int h = 0; h < N; h++)
-  {
-    for (int w = 0; w < N; w++)
-    {
-      char C = V[h][w];
-      if (C == 'A')
-      {
-        row_remainings[h].erase('A');
-        column_remainings[w].erase('A');
-      }
-      if (C == 'B')
-      {
-        row_remainings[h].erase('B');
-        column_remainings[w].erase('B');
-      }
-      if (C == 'C')
-      {
-        row_remainings[h].erase('C');
-        column_remainings[w].erase('C');
-      }
-      if (C == '*')
-      {
-        // もしrow_remainingとcolumn_remainingの両方が少なくとも１つの要素を持っているならintersectionを取る
-        if (row_remainings[h].size() != 0 && column_remainings[w].size() != 0)
-        {
-          set<char> remaining_intersection;
-          set_intersection(
-              row_remainings[h].begin(),
-              row_remainings[h].end(),
-              column_remainings[w].begin(),
-              column_remainings[w].end(),
-              inserter(remaining_intersection, remaining_intersection.end()));
-          if (remaining_intersection.size() > 0)
-          {
-            V[h][w] = *remaining_intersection.begin();
-            row_remainings[h].erase(V[h][w]);
-            column_remainings[w].erase(V[h][w]);
-          }
-          else
-          {
-            V[h][w] = '.';
-          }
-        }
-        // どちらかの要素数が0なら0ではないほうから取る
-        else
-        {
-          if (row_remainings[h].size() != 0)
-          {
-            V[h][w] = *row_remainings[h].begin();
-            row_remainings[h].erase(V[h][w]);
-            column_remainings[w].erase(V[h][w]);
-          }
-          else if (column_remainings[w].size() != 0)
-          {
-            V[h][w] = *column_remainings[w].begin();
-            row_remainings[h].erase(V[h][w]);
-            column_remainings[w].erase(V[h][w]);
-          }
-          else
-          {
-            V[h][w] = '.';
-          }
-        }
-      }
     }
   }
-  return V;
 }
 
-void print_res(vector<vector<char>> V)
+vector<vector<char>> create_patterns(int n, char head)
 {
-  bool ok = true;
-  int N = V.size();
-  for (int h = 0; h < N; h++)
+  vector<vector<char>> ret;
+  vector<char> CHARS = {'A', 'B', 'C', '.', '.'};
+  vector<char> chars_to_use(n);
+  copy(CHARS.begin(), CHARS.begin() + n, chars_to_use.begin());
+  do
   {
-    for (int w = 0; w < N; w++)
-    {
-      if (V[h][w] == '.')
-        ok = false;
-    }
-  }
-  if (ok)
-  {
-    cout << "Yes" << endl;
-    print_2vec(V);
-  }
-  else
-  {
-    cout << "No" << endl;
-    print_2vec(V);
-  }
-}
-
-struct dp_state
-{
-  bool column_resolved;
-  bool row_resolved;
+    // TODO:chars_to_useが候補の並びか判定してpush_backするか決める
+    ret.push_back(chars_to_use);
+  } while (next_permutation(chars_to_use.begin(), chars_to_use.end()));
+  return ret;
 };
 
 int main()
 {
   int N;
+  cin >> N;
   string R, C;
-  cin >> N >> R >> C;
-  vector<vector<dp_state>> dp(N + 1, vector<dp_state>(N + 1, {false, false}));
-  vector<vector<char>> output_map(N + 1, vector<char>(N + 1));
-  for (int h = 0; h < N; h++)
-  {
-    for (int w = 0; w < N; w++)
-    {
-      char R_char = R[h];
-      char C_char = C[w];
-      dp_state left_state = dp[h + 1][w];
-      dp_state upper_state = dp[h][w + 1];
-      if (left_state.row_resolved && upper_state.column_resolved)
-      {
-        output_map[h][w] = '*';
-        dp[h + 1][w + 1].row_resolved = true;
-        dp[h + 1][w + 1].column_resolved = true;
-      }
-      else if (!left_state.row_resolved && upper_state.column_resolved)
-      {
-        output_map[h][w] = R_char;
-        dp[h + 1][w + 1].row_resolved = true;
-        dp[h + 1][w + 1].column_resolved = true;
-      }
-      else if (left_state.row_resolved && !upper_state.column_resolved)
-      {
-        output_map[h][w] = C_char;
-        dp[h + 1][w + 1].row_resolved = true;
-        dp[h + 1][w + 1].column_resolved = true;
-      }
-      else if (!left_state.row_resolved && !upper_state.column_resolved)
-      {
-        if (R_char == C_char)
-        {
-          output_map[h][w] = R_char;
-          dp[h + 1][w + 1].row_resolved = true;
-          dp[h + 1][w + 1].column_resolved = true;
-        }
-        else
-        {
-          output_map[h][w] = '.';
-          dp[h + 1][w + 1].row_resolved = false;
-          dp[h + 1][w + 1].column_resolved = false;
-        }
-      }
-    }
-  }
-  output_map = write_map(output_map);
-  print_res(output_map);
+  cin >> R >> C;
+  vector<vector<char>> patterns = create_patterns(N, 'a');
+  print_2_vec(patterns);
+  return 0;
 }
---
+++ kyopro/src/AtCoder/acLibraryTest.cpp
@@ -0,0 +1,19 @@
+#include <atcoder/math>
+#include <iostream>
+
+/*
+  # command to complie
+  ```sh
+  g++ acLibraryTest.cpp -std=c++17 -I /usr/bin/ac-library
+  ```
+*/
+
+using namespace std;
+using namespace atcoder;
+
+int main()
+{
+  cout << pow_mod(2, 2, 3) << endl;
+
+  return 0;
+}
