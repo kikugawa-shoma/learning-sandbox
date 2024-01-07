ac85f35440a98721d303f30025c3f10ef10b0b5c
Shoma Kikugawa <kikugawa.s.shukatsu@gmail.com>
2024-01-07 23:53:32 +0900

    add kyopro utility

--- kyopro/src/myLibrary/cpp/utils.cpp
+++ kyopro/src/myLibrary/cpp/utils.cpp
@@ -4,7 +4,35 @@
 using namespace std;
 
 // long longの省略
-#define ll long long;
+#define ll long long
+// repマクロ
+#define rep(i, n) for (ll i = 0; i < N; i++)
+// main関数省略
+#define im int main()
+
+/**
+ * 入力受け取り
+ * 形式例："3"
+ */
+template <typename T>
+void inputSca(T &a)
+{
+  cin >> a;
+}
+
+/**
+ * 入力受け取り
+ * 形式例："1 2 3 ..."
+ */
+template <typename T>
+void inputVec(vector<T> &A)
+{
+  size_t N = A.size();
+  rep(i, N)
+  {
+    cin >> A[i];
+  }
+}
 
 /**
  * vectorを空白区切りで一行で出力(最後に改行)
@@ -13,7 +41,7 @@ template <typename T>
 void printVec(vector<T> &A)
 {
   size_t N = A.size();
-  for (int i = 0; i < N; i++)
+  rep(i, N)
   {
     cout << A[i];
     if (i != N - 1)
@@ -31,4 +59,12 @@ template <typename T>
 void printSca(T A)
 {
   cout << A << endl;
+}
+
+/**
+ * main関数の雛形
+ */
+im
+{
+  return 0;
 }
