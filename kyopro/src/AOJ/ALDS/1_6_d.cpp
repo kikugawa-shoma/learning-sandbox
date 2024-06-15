5dc8d8c130594445b3bc8098009bb725e6e9ebaf
Shoma Kikugawa <kikugawa.s.shukatsu@gmail.com>
2024-06-15 09:34:46 +0900

    Add kyopro

---
+++ kyopro/src/AOJ/ALDS/1_6_d.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n, buf;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &buf);
+    v[i] = buf;
+  }
+
+  return 0;
+}
